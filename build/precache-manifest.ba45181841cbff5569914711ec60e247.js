320742ef6619f63354f18a7a755c7e7136da1260
pankaj-pant <pankajpant89@gmail.com>
2019-11-11 22:18:20 +0200

    uibuild

---
+++ build/precache-manifest.ba45181841cbff5569914711ec60e247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee23acbff0b1e891989bb43f937f185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd7ab2cf7b7e0378ce82",
+    "url": "/static/css/main.c8347419.chunk.css"
+  },
+  {
+    "revision": "66f25210f51d65ba228d",
+    "url": "/static/js/2.a3c4a2c9.chunk.js"
+  },
+  {
+    "revision": "fd7ab2cf7b7e0378ce82",
+    "url": "/static/js/main.b90636d9.chunk.js"
+  },
+  {
+    "revision": "4e646f0e581801a589fc",
+    "url": "/static/js/runtime-main.7767e197.js"
+  }
+]);
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(morgan('tiny'))
 
 app.use(cors())
 
+app.use(express.static('build'))
 
 let persons = [
     {
