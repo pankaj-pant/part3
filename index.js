e6199cb3eef87eb94b9db6b8a8a4bfa7aa03e284
pankaj-pant <pankajpant89@gmail.com>
2019-11-15 19:15:30 +0200

    v16

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-require('dotenv').config({path: './.env'})
+require('dotenv').config()
 const bodyParser = require('body-parser')
 
 const Person = require('./models/person')
