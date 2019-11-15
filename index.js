ab3824689b13fa4d82d6730dceb6da30f498ab3d
pankaj-pant <pankajpant89@gmail.com>
2019-11-15 18:13:44 +0200

    v14

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ let persons = [
   
   app.use(errorHandler)
   
-  const PORT = process.env.PORT
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
